8fab792a58df806adf4876f6ffe40becef221ed3
ThatMrJ <116615393+ThatMrJ@users.noreply.github.com>
2025-08-20 20:41:03 +1200

    MVP

---
+++ 2d-game-test/scripts/game_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var score_label: Label = $ScoreLabel
+
+var score = 0
+
+func add_point():
+	score += 1
+	print(score)
+	score_label.text = "You collected " + str(score) + " coins."
--- 2d-game-test/scripts/player.gd
+++ 2d-game-test/scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,12 +13,29 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	# This gets the input direction -1 0 or 1, 0 means no direction, -1 and 1 mean left/right
+	
+	# Flip the Sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("Idle")
+		else:
+			animated_sprite.play("Run")
+	else:
+		animated_sprite.play("Jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
